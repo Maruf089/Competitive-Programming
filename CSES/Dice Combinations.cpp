1ac9be08f9ca4d95ac3dd45ba80b2e3edf1a8306
Maruf089 <maruf.cuet.cse16@gmail.com>
2019-11-16 13:06:18 +0600

    CPP

---
+++ CSES/Dice Combinations.cpp
@@ -0,0 +1,101 @@
+/**Bismillahir Rahmanir Rahim.**/
+#include<bits/stdc++.h>
+#define ln '\n'
+#define pf printf
+#define sf scanf
+#define inp(x) scanf("%lld",&x)
+#define D(x) cout<< #x " = "<<x<<endl
+#define foreach(i,c) for(__typeof(c.begin())i=c.begin();i!=c.end();i++)
+
+#define f0(i,b) for(int i=0;i<(b);i++)
+#define f1(i,b) for(int i=1;i<=(b);i++)
+#define _case cout<<"Case "<<++cs<<": "
+
+#define MOD 1000000007
+#define countv(v,a) count(v.begin(),v.end(),a)
+#define grtsrt(v) sort(v.begin(),v.end(),greater<int>())
+#define mnv(v) *min_element(v.begin(),v.end())
+#define mxv(v) *max_element(v.begin(),v.end())
+#define uniq(v) v.resize( unique(all(v)) - v.begin())
+
+#define PI acos(-1)
+#define ll long long int
+#define ull unsigned long long
+
+#define vi vector <int>
+#define vll vector <ll>
+#define vs vector <string>
+#define pb push_back
+#define eb emplace_back
+#define mpii map <int,int>
+#define mpsi map <string,int>
+#define mpll map <long long,long long>
+#define MP make_pair
+#define F first
+#define S second
+
+#define GCD(a,b) __gcd(a,b)
+#define LCM(a,b) (a*(b/__gcd(a,b)))
+
+#define rev(v) reverse(v.begin(),v.end())
+#define srt(v) sort(v.begin(),v.end())
+
+#define all(v) v.begin(),v.end()
+#define MEM(a, b) memset(a, b, sizeof(a))
+
+#define fast ios_base::sync_with_stdio(false)
+using namespace std;
+
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const long double EPS = 1e-9;
+const int inf = 0x3f3f3f3f;
+/// count set bit C = (num * 0x200040008001ULL & 0x111111111111111ULL) % 0xf; /// 32 bit integer
+
+/*------------------------------Graph Moves----------------------------*/
+//Rotation: S -> E -> N -> W
+//const int fx[] = {0, +1, 0, -1};
+//const int fy[] = {-1, 0, +1, 0};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*---------------------------------------------------------------------*/
+
+//Bit Operations
+inline bool checkBit(ll n, int i) { return n&(1LL<<i); }
+inline ll setBit(ll n, int i) { return n|(1LL<<i); }
+inline ll resetBit(ll n, int i) { return n&(~(1LL<<i)); }
+
+
+inline bool isSquare(ll x)  { ll s = sqrt(x); return (s*s==x); }
+inline bool isFib(ll x) { return isSquare(5*x*x+4)|| isSquare(5*x*x-4); }
+inline bool isPowerOfTwo(ll x)   { return ((1LL<<(ll)log2(x))==x); }
+
+typedef struct {
+    double x,y;
+   /// Point() {}
+  ///  Point(double x,double y) : x(x), y(y) {}
+} Point;
+
+double distance(Point a , Point b) {
+    return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+}
+
+string s,s1;
+ll n,m,a,b,i,j,d,t,cs=0,counT=0,k,ans=0,l=0,sum1=0,sum=0,Max,Min,num;
+
+map<ll,ll>mp;
+
+int main()
+{
+  cin >> n ;
+  ll dp[n+9];
+  dp[0] = dp[1] = 1 ; dp[2] = 2 ;
+  dp[3] = 4 ; dp[4] = 8 ;dp[5] = 16;
+  for(i=6;i<=n;i++)
+  {
+      dp[i] = (dp[i-1]+ dp[i-2] +dp[i-3]+ dp[i-4] + dp[i-5]+dp[i-6])%MOD;
+  }
+cout << dp[n]%MOD << endl;
+
+}
---
+++ CSES/Minimizing Coins1.cpp
@@ -0,0 +1,150 @@
+/**Bismillahir Rahmanir Rahim.**/
+#include<bits/stdc++.h>
+#define ln '\n'
+#define pf printf
+#define sf scanf
+#define inp(x) scanf("%lld",&x)
+#define D(x) cout<< #x " = "<<x<<endl
+#define foreach(i,c) for(__typeof(c.begin())i=c.begin();i!=c.end();i++)
+
+#define f0(i,b) for(int i=0;i<(b);i++)
+#define f1(i,b) for(int i=1;i<=(b);i++)
+#define _case cout<<"Case "<<++cs<<": "
+
+#define MOD 1000000007
+#define countv(v,a) count(v.begin(),v.end(),a)
+#define grtsrt(v) sort(v.begin(),v.end(),greater<int>())
+#define mnv(v) *min_element(v.begin(),v.end())
+#define mxv(v) *max_element(v.begin(),v.end())
+#define uniq(v) v.resize( unique(all(v)) - v.begin())
+
+#define PI acos(-1)
+#define ll long long int
+#define ull unsigned long long
+
+#define vi vector <int>
+#define vll vector <ll>
+#define vs vector <string>
+#define pb push_back
+#define eb emplace_back
+#define mpii map <int,int>
+#define mpsi map <string,int>
+#define mpll map <long long,long long>
+#define MP make_pair
+#define F first
+#define S second
+
+#define GCD(a,b) __gcd(a,b)
+#define LCM(a,b) (a*(b/__gcd(a,b)))
+
+#define rev(v) reverse(v.begin(),v.end())
+#define srt(v) sort(v.begin(),v.end())
+
+#define all(v) v.begin(),v.end()
+#define MEM(a, b) memset(a, b, sizeof(a))
+
+#define fast ios_base::sync_with_stdio(false)
+using namespace std;
+
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const long double EPS = 1e-9;
+const int inf = 0x3f3f3f3f;
+/// count set bit C = (num * 0x200040008001ULL & 0x111111111111111ULL) % 0xf; /// 32 bit integer
+
+/*------------------------------Graph Moves----------------------------*/
+//Rotation: S -> E -> N -> W
+//const int fx[] = {0, +1, 0, -1};
+//const int fy[] = {-1, 0, +1, 0};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*---------------------------------------------------------------------*/
+
+//Bit Operations
+inline bool checkBit(ll n, int i)
+{
+    return n&(1LL<<i);
+}
+inline ll setBit(ll n, int i)
+{
+    return n|(1LL<<i);
+}
+inline ll resetBit(ll n, int i)
+{
+    return n&(~(1LL<<i));
+}
+
+
+inline bool isSquare(ll x)
+{
+    ll s = sqrt(x);
+    return (s*s==x);
+}
+inline bool isFib(ll x)
+{
+    return isSquare(5*x*x+4)|| isSquare(5*x*x-4);
+}
+inline bool isPowerOfTwo(ll x)
+{
+    return ((1LL<<(ll)log2(x))==x);
+}
+
+typedef struct
+{
+    double x,y;
+    /// Point() {}
+    ///  Point(double x,double y) : x(x), y(y) {}
+} Point;
+
+double distance(Point a, Point b)
+{
+    return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+}
+
+string s,s1;
+ll n,m,a,b,i,j,d,t,cs=0,counT=0,k,ans=0,l=0,sum1=0,sum=0,Max,Min,num;
+
+map<ll,ll>mp;
+
+int main()
+{
+    cin >> n >> m ;
+    vector<ll>vc;
+
+    for(i=0;i<n;i++)
+        {
+            cin >> a;
+            vc.pb(a);
+        }
+
+
+ll dp[m+9];
+    for(i=1; i<=m; i++)
+        dp[i] = INF ;
+dp[0] = 0 ;
+    for(i=1; i<=m; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+           if(i-vc[j] >= 0 )
+             dp[i] =  min(dp[i-vc[j]]+1,dp[i]) ;
+        }
+    }
+
+//    for(i=0;i<=n;i++)
+//    {
+//    for(j=0;j<=m;j++)
+//        {
+//            cout << dp[i][j] << "  ";
+//        }
+//        cout << ln ;
+//    }
+
+
+if(dp[m] != INF )
+    cout << dp[m] << endl;
+    else cout << -1 << endl;
+
+}
+
