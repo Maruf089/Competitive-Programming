438244806722360ce1b346aeb22160dc13f98ca4
Maruf Hasan <maruf.cuet.cse16@gmail.com>
2020-05-04 15:50:47 +0600

    Add files via upload

---
+++ GYM/The 11th Zhejiang Provincial Collegiate Programming Contest/G.cpp
@@ -0,0 +1,358 @@
+///*بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم*///
+
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization ("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ln '\n'
+#define inp(x) scanf("%lld",&x)
+#define inp2(a,b) scanf("%lld %lld",&a,&b)
+
+#define f0(i,b) for(int i=0;i<(b);i++)
+#define f1(i,b) for(int i=1;i<=(b);i++)
+
+#define MOD 1000000007
+#define PI acos(-1)
+#define ll long long int
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+#define pb push_back
+#define F first
+#define S second
+#define sz size()
+#define LCM(a,b) (a*(b/__gcd(a,b)))
+//#define harmonic(n) 0.57721566490153286060651209+log(n)+1.0/(2*n)
+#define SORT_UNIQUE(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define all(v) v.begin(),v.end()
+#define MEM(a, b) memset(a, b, sizeof(a))
+
+#define fastio ios_base::sync_with_stdio(false)
+
+///Inline functions
+
+inline bool EQ(double a, double b)
+{
+    return fabs(a-b) < 1e-9;
+}
+//inline bool isLeapYearll year) { return (year%400==0) | (year%4==0 && year%100!=0); }
+inline void normal(ll &a)
+{
+    a %= MOD;
+    (a < 0) && (a += MOD);
+}
+inline ll modMul(ll a, ll b)
+{
+    a %= MOD, b %= MOD;
+    normal(a), normal(b);
+    return (a*b)%MOD;
+}
+inline ll modAdd(ll a, ll b)
+{
+    a %= MOD, b %= MOD;
+    normal(a), normal(b);
+    return (a+b)%MOD;
+}
+inline ll modSub(ll a, ll b)
+{
+    a %= MOD, b %= MOD;
+    normal(a), normal(b);
+    a -= b;
+    normal(a);
+    return a;
+}
+inline ll modPow(ll b, ll p)
+{
+    ll r = 1;
+    while(p)
+    {
+        if(p&1)
+            r = modMul(r, b);
+        b = modMul(b, b);
+        p >>= 1;
+    }
+    return r;
+}
+inline ll modInverse(ll a)
+{
+    return modPow(a, MOD-2);
+}
+inline ll modDiv(ll a, ll b)
+{
+    return modMul(a, modInverse(b));
+}
+inline bool isInside(pii p,ll n,ll m)
+{
+    return (p.first>=0&&p.first<n&&p.second>=0&&p.second<m);
+}
+inline bool isInside(pii p,ll n)
+{
+    return (p.first>=0&&p.first<n&&p.second>=0&&p.second<n);
+}
+inline bool isSquare(ll x)
+{
+    ll s = sqrt(x);
+    return (s*s==x);
+}
+inline bool isFib(ll x)
+{
+    return isSquare(5*x*x+4)|| isSquare(5*x*x-4);
+}
+inline bool isPowerOfTwo(ll x)
+{
+    return ((1LL<<(ll)log2(x))==x);
+}
+
+
+/// DEBUG --------------------------------------------------------------------------------->>>>>>
+
+///**
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p )
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v )
+{
+    os << "{";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if ( it != v.begin() )
+            os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v )
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if ( it != v.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v )
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if ( it != v.begin() )
+            os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+
+void faltu ()
+{
+    cerr << endl;
+}
+
+template <typename T>
+void faltu( T a[], int n )
+{
+    for (int i = 0; i < n; ++i)
+        cerr << a[i] << ' ';
+    cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest)
+{
+    cerr << arg << ' ';
+    faltu(rest...);
+}
+
+/// TEMPLATE ----------------------------------------------------------------------->>>>>>
+
+
+struct func
+{
+//this is a sample overloading function for sorting stl
+    bool operator()(pii const &a, pii const &b)
+    {
+        if(a.F==b.F)
+            return (a.S<b.S);
+        return (a.F<b.F);
+    }
+};
+/*------------------------------Graph Moves----------------------------*/
+//Rotation: S -> E -> N -> W
+//const int fx[] = {0, +1, 0, -1};
+//const int fy[] = {-1, 0, +1, 0};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*---------------------------------------------------------------------*/
+
+/// Bit Operations
+/// count set bit C = (num * 0x200040008001ULL & 0x111111111111111ULL) % 0xf; /// 32 bit integer
+/// inline bool checkBit(ll n, int i) { return n&(1LL<<i); }
+/// inline ll setBit(ll n, int i) { return n|(1LL<<i); }
+/// inline ll resetBit(ll n, int i) { return n&(~(1LL<<i)); }
+
+
+/// ************************************** Code starts here ****************************************** */
+
+
+int precedence(char op) {
+  if (op == '+' || op == '-')
+    return 1;
+  if (op == '*' || op == '/' || op == '%')
+    return 2;
+  return 0;
+}
+
+// Function to perform arithmetic operations.
+int applyOp(int a, int b, char op) {
+  switch (op) {
+  case '+': return a + b;
+  case '-': return a - b;
+  case '*': return a * b;
+  case '%': return a % b;
+  case '/': return a / b;
+  }
+  return 1;
+}
+
+// Function that returns value of
+// expression after evaluation.
+int evaluate(string tokens) {
+  //dbg(tokens);
+  int i;
+
+  // stack to store integer values.
+  stack <int> values;
+
+  // stack to store operators.
+  stack <char> ops;
+
+  for (i = 0; i < (int) tokens.length(); i++) {
+
+    // Current token is a whitespace,
+    // skip it.
+    if (tokens[i] == ' ')
+      continue;
+
+    // Current token is an opening
+    // brace, push it to 'ops'
+    else if (tokens[i] == '(') {
+      ops.push(tokens[i]);
+    }
+
+    // Current token is a number, push
+    // it to stack for numbers.
+    else if (isdigit(tokens[i])) {
+      int val = 0;
+
+      // There may be more than one
+      // digits in number.
+      while (i < (int) tokens.length() &&
+             isdigit(tokens[i]))
+      {
+        val = (val * 10) + (tokens[i] - '0');
+        i++;
+      }
+
+      values.push(val);
+    }
+
+    // Closing brace encountered, solve
+    // entire brace.
+    else if (tokens[i] == ')')
+    {
+      while (!ops.empty() && ops.top() != '(')
+      {
+        int val2 = values.top();
+        values.pop();
+
+        int val1 = values.top();
+        values.pop();
+
+        char op = ops.top();
+        ops.pop();
+
+        values.push(applyOp(val1, val2, op));
+      }
+
+      // pop opening brace.
+      if (!ops.empty())
+        ops.pop();
+    }
+
+    // Current token is an operator.
+    else
+    {
+      // While top of 'ops' has same or greater
+      // precedence to current token, which
+      // is an operator. Apply operator on top
+      // of 'ops' to top two elements in values stack.
+      while (!ops.empty() && precedence(ops.top())
+             >= precedence(tokens[i])) {
+        int val2 = values.top();
+        values.pop();
+
+        int val1 = values.top();
+        values.pop();
+
+        char op = ops.top();
+        ops.pop();
+
+        values.push(applyOp(val1, val2, op));
+      }
+
+      // Push current token to 'ops'.
+      ops.push(tokens[i]);
+    }
+  }
+
+  // Entire expression has been parsed at this
+  // point, apply remaining ops to remaining
+  // values.
+  while (!ops.empty()) {
+    int val2 = values.top();
+    values.pop();
+
+    int val1 = values.top();
+    values.pop();
+
+    char op = ops.top();
+    ops.pop();
+
+    values.push(applyOp(val1, val2, op));
+  }
+
+  // Top of 'values' contains result, return it.
+  return values.top();
+}
+int main()
+{
+
+  int T;
+  T = 1;
+  //scanf("%d", &T);
+  cin >> T;
+  cin.ignore();
+  for (int cs = 1; cs <= T; cs++) {
+    string s;
+    getline(cin, s);
+    cout << evaluate(s) << endl;
+  }
+  return 0;
+}
